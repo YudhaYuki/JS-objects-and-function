f7d07ad4409adab62e93e0abb57a9a3e81adb640
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-04 12:21:30 +0200

    Inheritance - PROTOTYPE

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 // function constructor
 
+/*
+// Normall Object
 var john = {
     name : 'John',
     yearOfBirth: 1990,
     job: 'teacher'
 };
+*/
 
 var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function() {
-        console.log(2016 - this.yearOfBirth);
-    }
 }
 
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+};
+
 var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
 
 john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
