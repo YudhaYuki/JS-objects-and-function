d05e35d95663d53591a63160bee2fcb273bdec01
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-05 15:20:28 +0200

    Closure PART II - Example II

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Closure
 
+// Example 1
+/*
 function retirement(retirementAge) {
     var a = ' years before retirement.';
     return function(yearOfBirth) {
@@ -9,6 +11,30 @@ function retirement(retirementAge) {
 }
 
 var retirementUS = retirement(66);
-retirementUS(1990);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
 
-retirement(66)(1990);
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+
+// retirement(66)(1990);
+*/
+
+
+
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ',  can you explain what is UX design?');            
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');            
+        } else {
+            console.log('Hello ' + name + ', what do you do?');            
+        }        
+    }
+}
+
+interviewQuestion('teacher')('John');
