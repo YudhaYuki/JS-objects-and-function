4c78f6341ce1183520c78d4f076bcc48f8403496
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-08 07:31:15 +0200

    Challange 4 - Answer I

---
+++ script-8.js
@@ -0,0 +1,69 @@
+
+// Bind, Call, and apply
+
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log(`Good ${timeOfDay}, Ladies and gentlemen! I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old.` );
+        } else if (style === 'friendly') {
+            console.log(`Hey, what's up? I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old, have a nice ${timeOfDay}`);
+        }   
+    }
+}
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+john.presentation('friendly', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+// It wont work, it just to show
+// john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('evening');
+
+
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+
+emilyFormal('afternoon');
+
+
+
+// Another example of BIND
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+    return el > limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+
+console.log(ages);
+console.log(fullJapan);
+
--- script.js
+++ script.js
@@ -1,68 +1,42 @@
-// Bind, Call, and apply
 
-var john = {
-    name: 'John',
-    age: 26,
-    job: 'teacher',
-    presentation: function(style, timeOfDay) {
-        if (style === 'formal') {
-            console.log(`Good ${timeOfDay}, Ladies and gentlemen! I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old.` );
-        } else if (style === 'friendly') {
-            console.log(`Hey, what's up? I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old, have a nice ${timeOfDay}`);
-        }   
-    }
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+
+function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
 }
-
-var emily = {
-    name: 'Emily',
-    age: 35,
-    job: 'designer'
-};
-
-john.presentation('friendly', 'morning');
-
-john.presentation.call(emily, 'friendly', 'afternoon');
-
-// It wont work, it just to show
-// john.presentation.apply(emily, ['friendly', 'afternoon']);
-
-
-var johnFriendly = john.presentation.bind(john, 'friendly');
-
-johnFriendly('morning');
-johnFriendly('evening');
-
-
-
-var emilyFormal = john.presentation.bind(emily, 'formal');
-
-emilyFormal('afternoon');
-
-
-
-// Another example of BIND
-
-var years = [1990, 1965, 1937, 2005, 1998];
-
-function arrayCalc(arr, fn) {
-    var arrRes = [];
-    for (i = 0; i < arr.length; i++) {
-        arrRes.push(fn(arr[i]));
-    }
-    return arrRes;
-}
-
-function calculateAge(el) {
-    return 2016 - el;
-}
-
-function isFullAge(limit, el) {
-    return el > limit;
-}
-
-var ages = arrayCalc(years, calculateAge);
-var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
-
-console.log(ages);
-console.log(fullJapan);
-
