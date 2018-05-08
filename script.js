03b515313084d22433a55d75665b6afb7a3f1194
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-08 10:34:06 +0200

    Challange 4 - Anser VIII and X

--- script.js
+++ script.js
@@ -123,12 +123,24 @@ c) correct answer (I would use a number for this)
 
 
     // Question VI
-    Question.prototype.checkAnswer = function(ans) {
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
+        
         if (ans === this.correct) {
             console.log('Correct answer')
+            sc = callback(true);
+
         } else {
             console.log('Wrong answer. TRY AGAIN !');
+            sc = callback(false);
         }
+
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('----------------------------------------------------------');
     }
 
 
@@ -149,6 +161,19 @@ c) correct answer (I would use a number for this)
     // Question III
     var questions = [q1, q2, q3];
 
+    // Question X
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+
+    var keepScore = score();
+
 
     // Question VIII
     function nextQuestion() {
@@ -168,7 +193,7 @@ c) correct answer (I would use a number for this)
         // Question IX
         if(answer !== 'exit') {
             // Question VI
-            questions[n].checkAnswer(parseInt(answer));
+            questions[n].checkAnswer(parseInt(answer), keepScore);
 
             nextQuestion();
         }
